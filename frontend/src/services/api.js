4b0fe4767c29c07547542a095ef6ff9617688dfd
varsha <varshavijayaragavan28@gmail.com>
2026-06-17 11:44:25 +0530

    Changed link in api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/atm";
+const API = "https://atm-application-j1fz.onrender.com/api/atm";
 
 export const signup = (data) => {
   return axios.post(`${API}/signup`, data);
